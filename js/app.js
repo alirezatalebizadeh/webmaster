2883a44740f8ff3d830d386c918a8f1341ba25f9
alireza talebizadeh <btc.lider78@gmail.com>
2022-09-09 22:29:46 +0430

    this is a personal site

---
+++ js/app.js
@@ -0,0 +1,88 @@
+
+function show() {
+    let elem = document.getElementById("input-travel").value;
+    if (elem == "travel") {
+        document.getElementById("travel-link").style.display = "inline";
+        document.getElementById("output").style.display = "none";
+        // document.getElementById("input-blockchain").value = "";
+        document.getElementById("form-travel").style.display = "none";
+
+    } else {
+        document.getElementById("output").style.color = "rgb(54, 53, 53)";
+        document.getElementById("input-travel").value = "";
+        // document.getElementById("blockchain-link").style.display = "none";
+        document.getElementById("output").innerHTML = "رمز وارد شده اشتباه است.لطفا از مدیر وب سایت رمز را دریافت نمایید";
+    }
+}
+
+
+function showinfo() {
+    let elem = document.getElementById("input-info").value;
+    if (elem == "information") {
+        document.getElementById("info-link").style.display = "inline";
+        document.getElementById("output-info").style.display = "none";
+        // document.getElementById("input-info").value = "";
+        document.getElementById("form-info").style.display = "none";
+    } else {
+        document.getElementById("output-info").style.color = "rgb(54, 53, 53)";
+        document.getElementById("input-info").value = "";
+        document.getElementById("output-info").innerHTML = "رمز وارد شده اشتباه است.لطفا از مدیر وب سایت رمز را دریافت نمایید";
+    }
+}
+
+
+function showrest() {
+    let elem = document.getElementById("input-rest").value;
+    if (elem == "restourantalireza") {
+        document.getElementById("link-rest").style.display = "inline";
+        document.getElementById("output-rest").style.display = "none";
+        // document.getElementById("input-rest").value = "";
+        document.getElementById("form-rest").style.display = "none";
+    } else {
+        document.getElementById("output-rest").style.color = "rgb(54, 53, 53)";
+        document.getElementById("input-rest").value = "";
+        document.getElementById("output-rest").innerHTML = "رمز وارد شده اشتباه است.لطفا از مدیر وب سایت رمز را دریافت نمایید";
+    }
+}
+
+
+function showblock() {
+    let elem = document.getElementById("input-blockchain").value;
+    if (elem == "blockchain") {
+        document.getElementById("link-block").style.display = "inline";
+        document.getElementById("output-block").style.display = "none";
+        // document.getElementById("input-rest").value = "";
+        document.getElementById("form-block").style.display = "none";
+    } else {
+        document.getElementById("output-block").style.color = "rgb(54, 53, 53)";
+        document.getElementById("input-blockchain").value = "";
+        document.getElementById("output-block").innerHTML = "رمز وارد شده اشتباه است.لطفا از مدیر وب سایت رمز را دریافت نمایید";
+    }
+}
+
+$(document).ready(function () {
+
+        $("#ul-navbar li").first().fadeIn(1000, function nextitem() {
+            $(this).next("li").fadeIn(1000, nextitem);
+        });
+    
+
+    $("#btn-job").click(function () {
+
+        $("#list-card div").first().fadeIn(1000, function nextitem() {
+            $(this).next("div").fadeIn(1000, nextitem);
+        });
+    });
+});
+
+
+
+
+
+
+
+
+
+
+
+
