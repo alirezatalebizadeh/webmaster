a09c2a6ddbc9c6e831626b41d910089a81a758d2
alireza talebizadeh <btc.lider78@gmail.com>
2022-09-12 02:26:01 +0430

    Solve some problems and add time and date

--- js/app.js
+++ js/app.js
@@ -71,3 +71,54 @@ triggerbutton.addEventListener("click", function () {
     }
 });
 
+
+
+function gregorian_to_jalali(gy, gm, gd) {
+    var g_d_m, jy, jm, jd, gy2, days;
+    g_d_m = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
+    gy2 = (gm > 2) ? (gy + 1) : gy;
+    days = 355666 + (365 * gy) + ~~((gy2 + 3) / 4) - ~~((gy2 + 99) / 100) + ~~((gy2 + 399) / 400) + gd + g_d_m[gm - 1];
+    jy = -1595 + (33 * ~~(days / 12053));
+    days %= 12053;
+    jy += 4 * ~~(days / 1461);
+    days %= 1461;
+    if (days > 365) {
+        jy += ~~((days - 1) / 365);
+        days = (days - 1) % 365;
+    }
+    if (days < 186) {
+        jm = 1 + ~~(days / 31);
+        jd = 1 + (days % 31);
+    } else {
+        jm = 7 + ~~((days - 186) / 30);
+        jd = 1 + ((days - 186) % 30);
+    }
+    return [jy, jm, jd];
+}
+
+MyDate = new Date();
+Year = MyDate.getFullYear();
+Month = MyDate.getMonth() + 1;
+Day = MyDate.getDate();
+
+var Result = document.getElementById("result");
+
+Tarikh_Shamci = gregorian_to_jalali(Year, Month, Day);
+Result.innerHTML = Tarikh_Shamci[0] + '/' + Tarikh_Shamci[1] + '/' + Tarikh_Shamci[2];
+
+
+
+
+function ShowTime() {
+    var MyDate = new Date();
+    var My_Hour = MyDate.getUTCHours();
+    var My_Minute = MyDate.getUTCMinutes();
+    var My_Second = MyDate.getUTCSeconds();
+    var Result = document.getElementById("result-time");
+    Result.innerHTML = My_Hour + ":" + My_Minute + ":" + My_Second ;
+}
+
+setInterval(ShowTime, 10);
+
+
+
